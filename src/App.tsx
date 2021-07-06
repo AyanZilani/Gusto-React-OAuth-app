e17eb99d733bd3564f8642b800166e8e8b28b541
AyanZilani <49856695+AyanZilani@users.noreply.github.com>
2021-07-06 12:36:00 +0600

    new changes in Ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import { Authorization } from './features/authorization/Authorization';
-import { SpotifyExample } from './features/spotifyExample/SpotifyExample';
-import './App.css';
+import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
+import Landing from './pages/Landing';
+import RedirectUrl from './pages/RedirectUrl';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Authorization />
-        <SpotifyExample />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className='container mx-auto my-10 px-6'>
+				<NavLink to='/' className='block text-3xl text-center font-semibold hover:underline'>
+					Connect to QuickBooks
+				</NavLink>
+				<Switch>
+					<Route exact path='/' component={Landing} />
+					<Route exact path='/redirectUrl' component={RedirectUrl} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
